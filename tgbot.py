81bb98e6940476ff36bd8a288c6e8a7d1ec4ceef
Alkir <111125825+AIk1r@users.noreply.github.com>
2023-08-09 12:07:44 +0700

    Create tgbot.py

---
+++ tgbot.py
@@ -0,0 +1,41 @@
+import asyncio
+import logging
+import os
+import re
+import requests
+
+from aiogram import Bot, types, Dispatcher, executor
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Bot token can be obtained via https://t.me/BotFather
+BOT_TOKEN = os.getenv('TELEGRAM_BOT')
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+
+def func_parser():
+    url = "Any site you have with html, not js"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    #This is where I use regex to find the information I need.
+    #I start by specifying the tag with the first argument and the second argument with the necessary data where to look for the information I need.
+    products = soup.find_all('a', {'href': re.compile(r"^/?course/.*")})
+
+    return products
+
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    products = stepik_parser()
+    course_titles = [product.text.strip() for product in products]
+    await message.answer(course_titles)
+    # for product in products:
+    #     await message.answer(product)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
