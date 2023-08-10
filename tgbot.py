9f0617adec342cb3ba2f7cd4717165aab98e4d8b
Alkir <111125825+AIk1r@users.noreply.github.com>
2023-08-10 13:06:02 +0700

    add product state, show all products and show first 5 products

--- tgbot.py
+++ tgbot.py
@@ -5,6 +5,13 @@ import re
 import requests
 
 from aiogram import Bot, types, Dispatcher, executor
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Command
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram import Bot, types, Dispatcher, executor
+
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
@@ -15,6 +22,11 @@ BOT_TOKEN = os.getenv('TELEGRAM_BOT')
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
+
+class ProductStates(StatesGroup):
+    MENU = State()
+    SHOW_ALL = State()
+    SHOW_FIRST_5 = State()
 
 def func_parser():
     url = "Any site you have with html, not js"
@@ -30,9 +42,29 @@ def func_parser():
 #This is the command itself for the bot, which will output the information we need when we enter it.
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    products = func_parser()
+    #Right now, the output buttons only work via a call to the '/start' command.
+    #That is, you will need to enter the command again to get the information again in some form
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.row(types.KeyboardButton("Show all products"), types.KeyboardButton("Show first 5 products"))
+    await message.answer("Hello! Please select one of the options:", reply_markup=kb)
+    await ProductStates.MENU.set()
+
+#When entering the command '/start' we will have two buttons, respectively
+#here we select the function to show all products and see all these products
+@dp.message_handler(lambda message: message.text == "Show all products", state=ProductStates.MENU)
+async def show_all_products(message: types.Message, state: FSMContext):
+    products = stepik_parser()
     course_titles = [product.text.strip() for product in products]
-    await message.answer(course_titles) #The output is implemented as a list
+    await message.answer(' '.join(course_titles))
+    await state.finish()
+
+#It's the same story here, but now we only get the first 5 elements of our product
+@dp.message_handler(lambda message: message.text == "Show first 5 products", state=ProductStates.MENU)
+async def show_first_5_products(message: types.Message, state: FSMContext):
+    products = stepik_parser()
+    course_titles = [product.text.strip() for product in products[:5]]
+    await message.answer(' '.join(course_titles))
+    await state.finish()
     
 
 if __name__ == '__main__':
